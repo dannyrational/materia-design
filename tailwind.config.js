4a6058cc9251504d97272bacdd93b4773f241fa1
Daniel Rodas <drodas6232@gmail.com>
2021-06-22 01:26:44 +0100

    3: Worked on Section 2 Mobile View

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   corePlugins: {
     container: false,
@@ -5,9 +7,41 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      rose: colors.rose,
+      pink: colors.pink,
+      fuchsia: colors.fuchsia,
+      purple: colors.purple,
+      violet: colors.violet,
+      indigo: colors.indigo,
+      blue: colors.blue,
+      sky: colors.sky,
+      cyan: colors.cyan,
+      teal: colors.teal,
+      emerald: colors.emerald,
+      green: colors.green,
+      lime: colors.lime,
+      yellow: colors.yellow,
+      amber: colors.amber,
+      orange: colors.orange,
+      red: colors.red,
+      warmGray: colors.warmGray,
+      trueGray: colors.trueGray,
+      gray: colors.gray,
+      coolGray: colors.coolGray,
+      blueGray: colors.blueGray,
+    },
     extend: {
+      height: {
+        '475': '475px',
+        '575': '575px',
+      },
       colors: {
-        'primary': '#746367',
+        'primary': '#756566',
         'secondary': '#1F232A',
       },
       maxWidth: {
